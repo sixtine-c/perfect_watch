cdd4dda24eda299a5fb3f73262389ca03aa59752
Sixtine Caspar <sixcaspar@gmail.com>
2021-11-24 17:44:34 +0100

    done movies random 10 with genres

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,20 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all.limit(10)
+    # @movies = Movie.all
+    @mood = Mood.find(22) #to change later
+    array_genres_id = MoodGenre.where(mood_id: @mood).pluck(:genre_id)
+    movie_ids = MovieGenre.where(genre_id: array_genres_id).pluck(:movie_id)
+    @movies_global = Movie.where(id: movie_ids)
+    @movies_top = @movies_global.where("rating > ?", 70).sample(7)
+    @movies_bof = @movies_global.sample(3)
+    @movies = @movies_top + @movies_bof
+    @movies = @movies.sample(10)
+
+
+
   end
+
+
+
 end
