1786c6ed3ebf167c160dff997fc71e884b822a8c
nb <nb.bischoff@gmail.com>
2021-11-29 14:51:08 +0100

    Mood

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -2,5 +2,5 @@ class Mood < ApplicationRecord
   has_many :mood_genres, dependent: :destroy
   has_many :mood_genre_exclusions, dependent: :destroy
   has_one_attached :photo
-  MOODS = ["Bière & Pizza", "What's in the box ?", "Retour vers le passé", "Kids friendly", "Ben & Jerry's (& Cry)", "I'm Going on an Adventure !", "Cocooning"]
+  MOODS = ["Bière & Pizza", "Cold Blood", "Time flies", "Kids friendly", "Ben & Jerry's (& Cry)", "I'm Going on an Adventure !", "Cocooning", "Not ready to sleep"]
 end
