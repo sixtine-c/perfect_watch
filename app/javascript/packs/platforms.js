2062dda1458e503dd0310c0b325573688d466ac9
Lilas Marchand <lilasviolettemarchand@gmail.com>
2021-11-30 10:30:48 +0100

    bouton platfrom done

--- app/javascript/packs/platforms.js
+++ app/javascript/packs/platforms.js
@@ -1,8 +1,23 @@
 const platformColor = () => {
-  const platform = document.getElementById('search_platform_netflix')
-  if (platform) {
-    platform.classList.remove('platform-btn')
-    platform.classList.add('platform-netflix')
+  const platformNetflix = document.getElementById('search_platform_netflix')
+  if (platformNetflix) {
+    platformNetflix.classList.remove('platform-btn')
+    platformNetflix.classList.add('platform-netflix')
+  }
+  const platformPrimeVideo = document.getElementById('search_platform_prime_video')
+  if (platformPrimeVideo) {
+    platformPrimeVideo.classList.remove('platform-btn')
+    platformPrimeVideo.classList.add('platform-prime-video')
+  }
+  const platformAppleTV = document.getElementById('search_platform_apple_tv')
+  if (platformAppleTV) {
+    platformAppleTV.classList.remove('platform-btn')
+    platformAppleTV.classList.add('platform-appletv')
+  }
+  const platformDisney = document.getElementById('search_platform_disney_')
+  if (platformDisney) {
+    platformDisney.classList.remove('platform-btn')
+    platformDisney.classList.add('platform-disney')
   }
 };
 
